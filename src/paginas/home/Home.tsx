97055135f3e029ecc0a75a72defc849d6cf4423a
Douglas Papani <douglas.papani@hotmail.com>
2024-04-10 16:45:31 -0300

    Adicioando Rotas, Home, Footer e NavBar

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -2,52 +2,25 @@ import React from 'react';
 import './Home.css';
 
 
-
 function Home() {
     return (
         <>
-            <header>
-            <div className='w-auto h-60 bg-gray-400 flex  justify-center items-start m-3'>
-                    <div className='basis-1/4 bg-red-500'>Header</div>
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+  
+              <div className="flex justify-around gap-4">
+              
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              </div>
             </div>
-            </header>
-            <div>
-                <h3 className='text-xl'>Body</h3>
-                <h5>justify-start Esquerda</h5>
-                <div className="flex flex-row  justify-start m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-                <h5>justify-end Direita</h5>
-                <div className="flex flex-row  justify-end m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-                <h5>justify-center Centro</h5>
-                <div className="flex flex-row  justify-center m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-                <h5>justify-around estremidades</h5>
-                <div className="flex flex-row  justify-between m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-                <h5>justify-around estremidades com espaçamento</h5>
-                <div className="flex flex-row  justify-around m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-
-            </div>
-
-            <footer>
-            <div className='w-auto h-60 bg-gray-400 flex  justify-center items-start m-3'>
-                    <div className='basis-1/4 bg-red-500'>Footer</div>
-            </div>
-            </footer>
-
-        </>
+  
+          </div>
+        </div>
+      
+      </>
     );
 }
 
